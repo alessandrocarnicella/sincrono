9b331e9e69014c56d6fcd9d8ace3443f03801dec
francar93 <carusofrancesc1993@libero.it>
2019-03-15 12:26:04 +0100

    Fix controller referente 4

--- sincrono/src/main/java/com/sincrono/corso/model/ReferenteService.java
+++ sincrono/src/main/java/com/sincrono/corso/model/ReferenteService.java
@@ -9,6 +9,4 @@ public interface ReferenteService extends JpaRepository<Referente, Integer> {
 	@Query("SELECT r.idRef FROM Referente r WHERE r.azienda = :nomeAzienda")
 	int findIdRefByAziendaName(@Param("nomeAzienda") Azienda nomeAzienda);
 
-	@Query("SELECT r.idRef FROM Azienda a, Referente r where a.nomeAzienda = :nomeAzienda")
-	int findIdRefByAziendaNameString(@Param("nomeAzienda") String nomeAzienda);
 }
