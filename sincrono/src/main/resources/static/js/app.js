54b03e5d59f7a818dba5539ad141548f6e6659e2
DeVitaGi <DeVitaGi@vt-devitagi.valueteam.com>
2019-03-20 14:49:34 +0100

    fix

--- sincrono/src/main/resources/static/js/app.js
+++ sincrono/src/main/resources/static/js/app.js
@@ -161,8 +161,6 @@ $(document).ready(function(){
 	
 		$("#modal-edit-commesse input[name=idCommessa]").val(id);
 		$("#modal-edit-commesse input[name=nomeCommessa]").val(nome);
-//		$("#modal-edit-commesse input[name=idDipendente]").val(dipendente);
-//		$("#modal-edit-commesse input[name=nomeAziendaCommessa]").val(aziendac);
 		$("#modal-edit-commesse input[name=tariffaCliente]").val(tariffa);
 		
 		$('#select-option-commessadip option').each(function(){ 
